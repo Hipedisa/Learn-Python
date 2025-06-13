43366eec0c0c2462a447bac4eb74c9f92bb4236d
Hipedisa3 <ahossam127@outlook.com>
2025-06-13 15:51:16 +0300

    renamed to do list and made phone num finder

--- hello.py
+++
@@ -1,8 +0,0 @@
-def main():
-    first, last = input("enter your full name nigga: ").strip().title().split(" ")
-    hello(first)
-
-def hello(to):
-    print(f"hello, {to}!")
-
-main()
--- scripts/hello.bat
+++
@@ -1,2 +0,0 @@
-@py c:\users\ahmed\oneDrive\Desktop\dev\hello.py %*
-@pause
